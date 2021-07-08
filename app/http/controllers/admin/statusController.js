4b91b2cb3d15547873e146cee1e09da7a61b186a
umutcan <akturkumutcan58@gmail.com>
2021-07-08 17:32:34 +0300

    Initial commit

--- app/http/controllers/admin/statusController.js
+++ app/http/controllers/admin/statusController.js
@@ -1,20 +1,19 @@
 const Order = require('../../../models/order')
-function  statusController(){
-    return {
-        update(req,res){
-            Order.updateOne({_id: req.body.orderId},{status:req.body.status },(err,data)=>{
-                if(err){
 
-                   return res.redirect('/admin/orders')    
+function statusController() {
+    return {
+        update(req, res) {
+            Order.updateOne({_id: req.body.orderId}, { status: req.body.status }, (err, data)=> {
+                if(err) {
+                    return res.redirect('/admin/orders')
                 }
-                // Emit event
+                // Emit event 
                 const eventEmitter = req.app.get('eventEmitter')
-                eventEmitter.emit('orederUpdated',{id:req.body.orderId ,status: req.body.status})
-                res.redirect('/admin/orders')
+                eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
+                return res.redirect('/admin/orders')
             })
         }
     }
-
 }
 
 module.exports = statusController
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,105 +3,102 @@ import Noty from 'noty'
 import { initAdmin } from './admin'
 import moment from 'moment'
 
-let addToCart = document.querySelectorAll('.add-to-cart');
+let addToCart = document.querySelectorAll('.add-to-cart')
 let cartCounter = document.querySelector('#cartCounter')
-function updateCart (pizza){
-    axios.post('/update-cart', pizza).then(res=>{
-         cartCounter.innerText = res.data.totalQty
-         new Noty ({
-             type:'success',
-             timeout:1000,
-             text:'item added to  cart',
-             progressBar:false,
-             
-         }).show()
-    }).catch(err=>{
-        new Noty ({
-            type:'error',
-            timeout:1000,
-            text:'Something went wrong',
-            progressBar:false,
-            
-        }).show()
-    })
+
+function updateCart(pizza) {
+   axios.post('/update-cart', pizza).then(res => {
+       cartCounter.innerText = res.data.totalQty
+       new Noty({
+           type: 'success',
+           timeout: 1000,
+           text: 'Item added to cart',
+           progressBar: false,
+       }).show();
+   }).catch(err => {
+       new Noty({
+           type: 'error',
+           timeout: 1000,
+           text: 'Something went wrong',
+           progressBar: false,
+       }).show();
+   })
 }
-addToCart.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{
-        
-        let pizza = JSON.parse(btn.dataset.pizza)
-        updateCart (pizza)
-        
-    })
+
+addToCart.forEach((btn) => {
+   btn.addEventListener('click', (e) => {
+       let pizza = JSON.parse(btn.dataset.pizza)
+       updateCart(pizza)
+   })
 })
 
 // Remove alert message after X seconds
 const alertMsg = document.querySelector('#success-alert')
-if(alertMsg){
-    setTimeout(()=>{
-        alertMsg.remove()
-    },2000)
+if(alertMsg) {
+   setTimeout(() => {
+       alertMsg.remove()
+   }, 2000)
 }
 
 
-// change order status
-let statuses = document.querySelectorAll('.status_line')
-//console.log(statuses)
 
+// Change order status
+let statuses = document.querySelectorAll('.status_line')
 let hiddenInput = document.querySelector('#hiddenInput')
 let order = hiddenInput ? hiddenInput.value : null
 order = JSON.parse(order)
 let time = document.createElement('small')
-//console.log(order);
-// change order status
-function updateStatus(order){
-    statuses.forEach((status)=>{
-        status.classList.remove('step-completed')
-        status.classList.remove('current')
-    })
-    let stepCompleted = true;
-    statuses.forEach((status)=>{
-        let dataProp = status.dataset.status
-        if(stepCompleted){
-            status.classList.add('step-completed')
-        }
-        if(dataProp === order.status){
-            stepCompleted = false
-            time.innerText = moment(order.updateAt).format('hh:mm A')
-            status.appendChild(time)
-            if( status.nextElementSibling){
-                status.nextElementSibling.classList.add('current')
-            }
-            
 
-        }
-    })
+function updateStatus(order) {
+   statuses.forEach((status) => {
+       status.classList.remove('step-completed')
+       status.classList.remove('current')
+   })
+   let stepCompleted = true;
+   statuses.forEach((status) => {
+      let dataProp = status.dataset.status
+      if(stepCompleted) {
+           status.classList.add('step-completed')
+      }
+      if(dataProp === order.status) {
+           stepCompleted = false
+           time.innerText = moment(order.updatedAt).format('hh:mm A')
+           status.appendChild(time)
+          if(status.nextElementSibling) {
+           status.nextElementSibling.classList.add('current')
+          }
+      }
+   })
 
 }
 
 updateStatus(order);
 
-// Socket 
+
+
+// Socket
 let socket = io()
-initAdmin(socket)
-//Join
-if(order){
-    socket.emit('join',`order_${order._id}`)
+
+// Join
+if(order) {
+   socket.emit('join', `order_${order._id}`)
 }
-let adminAreaPath =window.location.pathname;
-if(adminAreaPath.includes('admin')){
-    socket.emit('join','adminRoom')
+let adminAreaPath = window.location.pathname
+if(adminAreaPath.includes('admin')) {
+   initAdmin(socket)
+   socket.emit('join', 'adminRoom')
 }
-socket.on('orderIpdated',(data)=>{
-    const updatedOrder = { ...order }
-    updatedOrder.updateAt = moment().format()
-    updatedOrder.status = data.status
-    updateStatus(updatedOrder)
-    new Noty ({
-        type:'success',
-        timeout:1000,
-        text:'order updated',
-        progressBar:false,
-        
-    }).show()
-    //console.log(data);
+
+
+socket.on('orderUpdated', (data) => {
+   const updatedOrder = { ...order }
+   updatedOrder.updatedAt = moment().format()
+   updatedOrder.status = data.status
+   updateStatus(updatedOrder)
+   new Noty({
+       type: 'success',
+       timeout: 1000,
+       text: 'Order updated',
+       progressBar: false,
+   }).show();
 })
